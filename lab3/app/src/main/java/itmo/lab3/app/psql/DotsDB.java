b4c1b7c1555113d611b4b8d09fb5a9cbf3e4eea4
ta4ilka69 <artem_balin_2004@mail.ru>
2023-11-22 00:31:51 +0300

    ??

--- lab3/app/src/main/java/itmo/lab3/app/psql/DotsDB.java
+++ lab3/app/src/main/java/itmo/lab3/app/psql/DotsDB.java
@@ -1,17 +1,30 @@
 package itmo.lab3.app.psql;
 
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import java.util.List;
 
 public class DotsDB {
-    private EntityManagerFactory emf;
-    public DotsDB() {
+    private final EntityManagerFactory emf;
+    private final String session;
+    public DotsDB(String session) {
         this.emf = Persistence.createEntityManagerFactory("MyPSQLConnection");
+        this.session = session;
     }
 
-    public List<Dot> getDots(String session) {
-        return emf.createEntityManager().createQuery("SELECT d FROM Dot d where session = ", Dot.class).getResultList();
+    public List<Dot> getDots() {
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<Dot> query = em.createQuery("SELECT d FROM Dot d WHERE d.session = :session", Dot.class);
+        List<Dot> dots = query.setParameter("session", session).getResultList();
+        em.close();
+        return dots;
+    }
+    public void addDot(Dot dot) {
+        emf.createEntityManager().getTransaction().begin();
+        emf.createEntityManager().persist(dot);
+        emf.createEntityManager().getTransaction().commit();
     }
 }
