5a53b00c38ac658c9f41e10cd7466b2ab27fad8e
ta4ilka69 <artem_balin_2004@mail.ru>
2023-12-19 07:20:35 +0300

    updated header and footer

--- front4/front/src/main/App.js
+++ front4/front/src/main/App.js
@@ -1,16 +1,34 @@
-import React from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom/BrowserRouter";
+import * as React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Login from "../components/Login";
 import Register from "../components/Register";
 import Dots from "../components/Dots";
+import Footer from "../main/Footer";
+import Header from "../main/Header"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        component: Dots,
+    },
+    {
+        path: "/login",
+        component: Login,
+    },
+    {
+        path: "/register",
+        component: Register,
+    }
+]);
 const App = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={Dots}/>
-            <Route exact path="/login" component={Login}/>
-            <Route exact path="/register" component={Register}/>
-        </Switch>
-    </Router>
+    <React.StrictMode>
+        <Header/>
+        <RouterProvider router={router} />
+        <Footer/>
+    </React.StrictMode>
 );
 
 export default App;
---
+++ front4/front/src/main/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="header">
+        <p>Artem Balin aka Ta4ilka69</p>
+        <p>№693</p>
+    </div>
+  );
+}
+export default Footer;
